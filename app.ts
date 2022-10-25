9a33f6c0e399b8e89d45597acddf3b3302b6101e
Nemanja Cenkovic <cenkovic.nemanja@gmail.com>
2022-10-25 11:22:02 +0200

    Express + typescript + eslint

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express, {Express} from 'express'
+import dotenv from 'dotenv'
+import logger from 'morgan'
+
+dotenv.config()
+const app: Express = express()
+const port = process.env.PORT
+
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/ping', (req, res) => {
+  res.json({ pong: true })
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
+
